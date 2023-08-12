33337892831de2693d1870187f99a5856a2f002b
atroopa <atroopa@gmail.com>
2023-08-12 08:09:06 -0700

    make struct and test it

--- 4_unmutble/main.go
+++ 4_unmutble/main.go
@@ -1,9 +1,25 @@
 package main
 
+// imports
 import (
 	"fmt"
 )
 
+// structs for make subjects
+type Artist struct {
+	Name, Genre string
+	Songs       int
+}
+
+// function added a step to aongs
+func newRelease(a Artist) int {
+	a.Songs++
+	return a.Songs
+}
+
+// main function
 func main() {
-	fmt.Println("hello")
+	me := Artist{Name: "Matt", Genre: "Electro", Songs: 42}
+	fmt.Printf("%s released their %d th songs \n", me.Name, newRelease(me))
+	fmt.Printf("%s has a total of %d songs \n", me.Name, me.Songs)
 }
