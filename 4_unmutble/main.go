1742971a6b4e4c34097c2e649c92a739a2b9ff33
atroopa <atroopa@gmail.com>
2023-08-12 08:21:01 -0700

    step 4 finish

--- 4_unmutble/main.go
+++ 4_unmutble/main.go
@@ -12,14 +12,15 @@ type Artist struct {
 }
 
 // function added a step to aongs
-func newRelease(a Artist) int {
+func newRelease(a *Artist) int {
 	a.Songs++
 	return a.Songs
 }
 
 // main function
 func main() {
-	me := Artist{Name: "Matt", Genre: "Electro", Songs: 42}
+	me := &Artist{Name: "Matt", Genre: "Electro", Songs: 42}
 	fmt.Printf("%s released their %d th songs \n", me.Name, newRelease(me))
 	fmt.Printf("%s has a total of %d songs \n", me.Name, me.Songs)
+	fmt.Println(&me)
 }
