d1f1e61ab47bd07a61d0d877a45bff8b536fa4e7
atroopa <atroopa@gmail.com>
2023-08-12 06:21:39 -0700

    learning for pointer

--- 3_pointer/main.go
+++ 3_pointer/main.go
@@ -14,9 +14,16 @@ func main() {
 	fmt.Println(family)
 	fmt.Printf("this is address of example: %p \n", &example)
 	fmt.Println("==============================")
-	example = "kirkos"
-	fmt.Println(example)
 
+	var num int = 42
+	var ptr *int
+
+	ptr = &num
+
+	fmt.Printf("Value of num: %d\n", num)
+	fmt.Printf("Address of num: %p\n", &num)
+	fmt.Printf("Value of ptr: %p\n", ptr)
+	fmt.Printf("Value pointed by ptr: %d\n", *ptr)
 }
 
 // functions
