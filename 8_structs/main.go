9a94e0f8addc80fb09b77df34f32909839ad8511
atroopa <atroopa@gmail.com>
2023-08-13 04:12:06 -0700

    perfesional sayhello function

--- 8_structs/main.go
+++ 8_structs/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // ===================== types =============================
 type NewStruct struct {
 	Name   string
 	Family string
+	Model  newModel
 }
 
 type Stringer interface {
+}
+
+type newModel struct {
+	ID        uint64
+	CreatedAt time.Time
 }
 
 // =============== main function ==========================
@@ -22,12 +30,16 @@ func main() {
 	fmt.Println("====== variables ==========")
 	fmt.Println(newStruct.Name)
 	fmt.Println(newStruct.Family)
+
 }
 
 // =============== functions ================================
 func (ns *NewStruct) SayHello(newName string, newFamily string) string {
 	ns.Name = newName
 	ns.Family = newFamily
-	result := fmt.Sprintf("Hello Name: %s Family: %s", ns.Name, ns.Family)
+	rand.Seed(time.Now().UnixNano())
+	ns.Model.ID = rand.Uint64()
+	ns.Model.CreatedAt = time.Now()
+	result := fmt.Sprintf("Hello Name: %s Family: %s id: %d time: %s", ns.Name, ns.Family, ns.Model.ID, ns.Model.CreatedAt)
 	return result
 }
