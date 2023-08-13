2d944c60ffa9703fb81bbf53aa4ba9613806a100
atroopa <atroopa@gmail.com>
2023-08-13 06:08:08 -0700

    2d Array learn

--- 9_composition/main.go
+++ 9_composition/main.go
@@ -19,9 +19,27 @@ type Player struct {
 // =========main====================
 
 func main() {
+
 	omid := &Player{}
 	omid.Name = "omid"
 	omid.PrintName()
+
+	var a [3]string
+	a[0] = "hello"
+	a[1] = "world"
+	a[2] = "!!"
+	fmt.Println(a)
+
+	b := [3]string{"hello", "World", "!!"}
+	fmt.Println(b)
+
+	c := [2][3]string{
+		{"this", "is", "first"},
+		{"this", "is", "second"},
+	}
+	fmt.Println(c[0])
+	fmt.Println(c[1])
+
 }
 
 // ========functions=================
